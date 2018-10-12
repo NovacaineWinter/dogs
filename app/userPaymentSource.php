3a21b043f3e680673bc73afc65e40c2d261e245f
Matt Hartley <matt.v.hartley@gmail.com>
2018-10-12 15:14:03 +0100

    bugfix;

--- app/userPaymentSource.php
+++ app/userPaymentSource.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class userPaymentSource extends Model
 {
 
-	protected $fillable ['stripe_id','lastFour','exp_month','exp_year'];
+	protected $fillable = ['stripe_id','lastFour','exp_month','exp_year'];
 
     public function user(){
     	return $this->belongsTo('\App\User','user_id');
